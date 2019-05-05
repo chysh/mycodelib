95b35aa2608784f66f779bfaac316dfb9451f495
chysh <yushuo7013@163.com>
2019-05-05 15:27:14 +0800

    将当前进程置于后台运行

---
+++ ProcToBackground.c
@@ -0,0 +1,27 @@
+VOID CurrentProcToBackground(char *workingPath)
+{
+	pid_t pid;
+  
+	if( (pid = fork()))
+		exit(0);
+	else if( pid < 0 )
+	{
+    printf("Fork Error: %s\n", strerror(errno) );
+		exit(1);
+	}
+
+	setsid();
+	if( (pid = fork()) )
+		exit(0);
+	else if( pid < 0 )
+	{
+    printf("Fork Error: %s\n", strerror(errno) );
+		exit(2);
+	}
+
+	chdir(workingPath);
+	umask(0);
+
+	signal(SIGTTOU, emptycall);
+	signal(SIGCHLD, SIG_IGN);	
+}
